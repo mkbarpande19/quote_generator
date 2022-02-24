60ef7155bed7c5021343bde2559a59b08bd8c97e
mayurbarpande19 <mayur.barpande19@gmail.com>
2022-02-24 16:53:01 +0530

    simple random quote generator

---
+++ src/Quote.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+const Quote = () => {
+    const [bgColor, setBgColor] = useState("#d3d3d3");
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    useEffect(() => {
+        getQuote();
+    }, []);
+
+    const getQuote = async () => {
+        await fetch("https://quotable.io/random").then((res) => res.json())
+            .then((data) => {
+                setQuote(data.content);
+                setAuthor(data.author);
+            }).catch((error) => console.log(error));
+    }
+
+    function generateRandomColor() {
+        let maxVal = 0xFFFFFF; // 16777215
+        let randomNumber = Math.random() * maxVal;
+        randomNumber = Math.floor(randomNumber);
+        randomNumber = randomNumber.toString(16);
+        let randColor = randomNumber.padStart(6, 0);
+        return `#${randColor.toUpperCase()}`
+    }
+
+    const getQuotesData = () => {
+        getQuote();
+        let color = generateRandomColor();
+        setBgColor(color);
+        document.body.style.backgroundColor = color;
+    }
+
+    return (
+        <div className='quoteWrapper'>
+            <div>
+                <div className='quote'>
+                    "{quote}"
+                </div>
+                <div className='author'>
+                    <span>{author}</span>
+                </div>
+            </div>
+            <div
+                className='nextQuoteButton' style={{ backgroundColor: bgColor }}
+                onClick={() => { getQuotesData() }}
+            >New Quote</div>
+        </div>
+    )
+}
+
+export default Quote
